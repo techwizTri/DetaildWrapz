c8effdcc025d89058af8b5ca49582627ecad636e
techwizTri <tri33hoang@gmail.com>
2023-11-20 22:12:33 -0800

    slider img

--- src/js/home.js
+++ src/js/home.js
@@ -1,19 +1,50 @@
-function handleSliderClick(clickedBtn) {
-  const sliderBtns = document.querySelectorAll(".slider-btn");
+// const handleSliderBtn = function (clickedBtn) {
+//   const sliderBtn = document.querySelectorAll(".slider-btn");
 
-  sliderBtns.forEach((btn) => {
-    if (btn === clickedBtn) {
-      btn.classList.add("active");
-    } else {
-      btn.classList.remove("active");
-    }
-  });
-}
+//   sliderBtn.forEach((btn) => {
+//     if (btn === clickedBtn) {
+//       clickedBtn.classList.add("active");
+//     } else {
+//       btn.classList.remove("active");
+//     }
+//   });
+// };
 
+// const sliderBtn = document.querySelectorAll(".slider-btn");
+// sliderBtn.forEach((btn) => {
+//   btn.addEventListener("click", function () {
+//     handleSliderBtn(this);
+//   });
+// });
+
+// JavaScript
+const images = [
+  "src/img/exotics/img1.jpg",
+  "src/img/exotics/img2.jpg",
+  "src/img/exotics/img3.jpg",
+  "src/img/exotics/img4.jpg",
+  "src/img/exotics/img5.jpg",
+];
+
+const heroImg = document.querySelector(".hero-img");
 const sliderBtns = document.querySelectorAll(".slider-btn");
 
-sliderBtns.forEach((btn) => {
+let currentImageIndex = 0;
+
+sliderBtns.forEach((btn, index) => {
   btn.addEventListener("click", function () {
-    handleSliderClick(this);
+    currentImageIndex = index;
+    updateHeroImage();
+    updateSliderButtons();
   });
 });
+
+function updateHeroImage() {
+  heroImg.src = images[currentImageIndex];
+}
+
+function updateSliderButtons() {
+  sliderBtns.forEach((btn, index) => {
+    btn.classList.toggle("active", index === currentImageIndex);
+  });
+}
