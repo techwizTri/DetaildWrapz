77bf3719f4315f4fba7a1e953432d45906105b02
techwizTri <tri33hoang@gmail.com>
2023-12-02 14:04:30 -0800

    js

--- src/js/home.js
+++ src/js/home.js
@@ -1,86 +1,35 @@
 "use script";
 
-// Master Branch
+document.addEventListener("DOMContentLoaded", function () {
+  let currentSlide = 1;
+  const totalSlides = 5;
 
-// const slides = [
-//   {
-//     title: "ADVANCED PAINT CORRECTION AND PROTECTIVE COATING",
-//     description:
-//       "We are the leading destination in Las Vegas for XPEL paint protection film, XPEL Prime XR Plus, and 3M Crystalline premium window tint. Additionally, we are certified in Ceramic Pro and Gyeon Quartz ceramic coatings, offering a comprehensive range of premium automotive services.",
-//     imageSrc: "src/img/exotics/img1.jpg",
-//   },
-//   {
-//     title: "HIGH-QUALITY CUSTOM COLOR TRANSFORMATION WITH VINYL WRAPPING",
-//     description:
-//       "We take great care in our comprehensive color-change programs to redefine the aesthetic of your high-performance and luxury vehicles, where achieving perfection is our singular objective.",
-//     imageSrc: "src/img/exotics/img2.jpg",
-//   },
-//   {
-//     title: "CERTIFIED CERAMIC COATING EXPERTS",
-//     description:
-//       "We support our ceramic coatings with certifications and warranties from Ceramic Pro and Gyeon Quartz, guaranteeing the genuine value of our services for you.",
-//     imageSrc: "src/img/exotics/img3.jpg",
-//   },
-//   {
-//     title: "COMPREHENSIVE CONCOURS DETAILING SOLUTIONS",
-//     description:
-//       "We approach vehicles with the same care as if they were our own, elevating our level of attention to go beyond your expectations and deliver more than anticipated.",
-//     imageSrc: "src/img/exotics/img4.jpg",
-//   },
-//   {
-//     title: "FULLY CUSTOMIZABLE PROTECTION PACKAGES",
-//     description:
-//       "We customize each service to match your preferences, providing you with the flexibility to choose the protection options that best suit your needs.",
-//     imageSrc: "src/img/exotics/img5.jpg",
-//   },
-// ];
+  // Function to check the next slide
+  function autoSlide() {
+    currentSlide = (currentSlide % totalSlides) + 1;
+    document.getElementById(`slide${currentSlide}`).checked = true;
+  }
 
-// const sliderContent = document.querySelector(".hero-text-box");
-// const sliderBtn = document.querySelectorAll(".slider-btn");
-// const heroContent = document.querySelector(".hero-container");
+  // Set interval to check the next slide every 5000 milliseconds (5 seconds)
+  let timer = setInterval(autoSlide, 3000);
 
-// let currentIndexSlide = 0;
-// let timer;
+  const labels = document.querySelectorAll("#bullets label");
 
-// sliderBtn.forEach((btn, index) => {
-//   btn.addEventListener("click", function () {
-//     resetTimer();
-//     currentIndexSlide = index;
-//     updateSlide();
-//     updateSlideBtn();
+  labels.forEach(function (label) {
+    label.addEventListener("click", function () {
+      console.log("xlixk");
+    });
+  });
+});
+
+//   const labels = document.querySelectorAll("#controls label");
+
+//   labels.forEach(function (label, index) {
+//     label.addEventListener("click", function () {
+//       clearInterval(intervalId);
+//       currentSlide = (index + 2) % totalSlides || totalSlides;
+//       document.getElementById(`slide${currentSlide}`).checked = true;
+//       startInterval();
+//     });
 //   });
 // });
-
-// function updateSlide() {
-//   const currentSlide = slides[currentIndexSlide];
-//   sliderContent.querySelector(".heading-primary").textContent =
-//     currentSlide.title;
-//   sliderContent.querySelector(".hero-description").textContent =
-//     currentSlide.description;
-//   heroContent.querySelector(".hero-img").src = currentSlide.imageSrc;
-// }
-
-// function updateSlideBtn() {
-//   sliderBtn.forEach((btn, index) => {
-//     btn.classList.toggle("active", index === currentIndexSlide);
-//   });
-// }
-
-// function nextSlide() {
-//   currentIndexSlide = (currentIndexSlide + 1) % slides.length;
-//   updateSlide();
-//   updateSlideBtn();
-// }
-
-// function startTimer() {
-//   timer = setInterval(nextSlide, 5000);
-// }
-
-// function resetTimer() {
-//   clearInterval(timer);
-//   startTimer();
-// }
-
-// updateSlide();
-
-// startTimer();
