ed6dc588a43e50eb4a13546ad5e9a51ac272700e
techwizTri <tri33hoang@gmail.com>
2023-12-02 14:05:24 -0800

    js

--- src/js/home.js
+++ src/js/home.js
@@ -166,3 +166,26 @@ function resetTimer() {
 updateSlide();
 
 startTimer();
+
+// TEST
+document.addEventListener("DOMContentLoaded", function () {
+  let currentSlide = 1;
+  const totalSlides = 5;
+
+  // Function to check the next slide
+  function autoSlide() {
+    currentSlide = (currentSlide % totalSlides) + 1;
+    document.getElementById(`slide${currentSlide}`).checked = true;
+  }
+
+  // Set interval to check the next slide every 5000 milliseconds (5 seconds)
+  let timer = setInterval(autoSlide, 3000);
+
+  const labels = document.querySelectorAll("#bullets label");
+
+  labels.forEach(function (label) {
+    label.addEventListener("click", function () {
+      console.log("xlixk");
+    });
+  });
+});
