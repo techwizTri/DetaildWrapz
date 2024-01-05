0a0c60edba31880e22273f7e0ea5b3f6420fd568
techwizTri <tri33hoang@gmail.com>
2024-01-04 21:45:24 -0800

    changed modal

---
+++ src/js/galllerypage2.js
@@ -0,0 +1,14 @@
+"use strict";
+// // Add these JavaScript functions to your existing script
+// function showImage(src) {
+//   // Display the modal
+//   document.getElementById("imageModal").style.display = "block";
+
+//   // Set the image source in the modal
+//   document.getElementById("modalImage").src = src;
+// }
+
+// function closeModal() {
+//   // Close the modal
+//   document.getElementById("imageModal").style.display = "none";
+// }
