dc68e206fb3ea74d2008f35d0864c471aba60eb1
techwizTri <tri33hoang@gmail.com>
2024-01-05 22:44:03 -0800

    modal

--- src/js/galllerypage2.js
+++ src/js/galllerypage2.js
@@ -12,11 +12,46 @@
 //   // Close the modal
 //   document.getElementById("imageModal").style.display = "none";
 // }
+// Makes the clicked image full screen (uses a <div> with a background image)
+document.querySelectorAll(".img-full-screen").forEach(function (element) {
+  element.addEventListener("click", function () {
+    // Prevents scrolling
+    document.body.classList.add("scroll-disabled");
 
-const gallery = document.querySelectorAll(".gallery-modal");
+    // Optional: Enables pinch and zoom
+    document
+      .querySelector("meta[name=viewport]")
+      .setAttribute(
+        "content",
+        "width=device-width, initial-scale=1.00, minimum-scale=1.00, maximum-scale=2.00, user-scalable=yes"
+      );
 
-gallery.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    console.log("clcked");
+    // Get image path source
+    let imagePath = this.getAttribute("src");
+
+    // Set image path source
+    document.querySelector(".img-placeholder").style.backgroundImage =
+      "url(" + imagePath + ")";
+
+    // Show image
+    document.querySelector(".img-placeholder").style.display = "block";
   });
 });
+
+document
+  .querySelector(".img-placeholder")
+  .addEventListener("click", function () {
+    // Enables scrolling again
+    document.body.classList.remove("scroll-disabled");
+
+    // Optional: Disables pinch and zoom
+    document
+      .querySelector("meta[name=viewport]")
+      .setAttribute(
+        "content",
+        "width=device-width, initial-scale=1.00, minimum-scale=1.00, maximum-scale=1.00"
+      );
+
+    // Hide image
+    this.style.display = "none";
+  });
