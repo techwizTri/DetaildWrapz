6765030c63111b3004a268f81dc6f6ef20b9bee4
techwizTri <tri33hoang@gmail.com>
2024-01-11 22:30:19 -0800

    image modal completed

--- src/js/galllerypage2.js
+++ src/js/galllerypage2.js
@@ -39,7 +39,6 @@ document.querySelector(".img-placeholder").appendChild(rightArrow);
 const leftArrow = document.createElement("div");
 leftArrow.className = "left-arrow";
 
-// Create an arrow SVG element
 const arrowLeftSvgElement = document.createElementNS(
   "http://www.w3.org/2000/svg",
   "svg"
@@ -51,7 +50,6 @@ arrowLeftSvgElement.setAttribute("stroke-width", "1.5");
 arrowLeftSvgElement.setAttribute("stroke", "currentColor");
 arrowLeftSvgElement.setAttribute("class", "w-6 h-6");
 
-// Create a path element inside the arrow SVG for the arrow icon
 const arrowLeftPathElement = document.createElementNS(
   "http://www.w3.org/2000/svg",
   "path"
@@ -60,96 +58,11 @@ arrowLeftPathElement.setAttribute("stroke-linecap", "round");
 arrowLeftPathElement.setAttribute("stroke-linejoin", "round");
 arrowLeftPathElement.setAttribute("d", "M15.75 19.5 8.25 12l7.5-7.5");
 
-// Append the path element to the arrow SVG
 arrowLeftSvgElement.appendChild(arrowLeftPathElement);
 
 leftArrow.appendChild(arrowLeftSvgElement);
 
-// Append the arrow SVG element to the body or any other desired container
 document.querySelector(".img-placeholder").appendChild(leftArrow);
-
-/**************************/
-/* Event Listeners for Arrows */
-/**************************/
-
-const test = document.querySelectorAll(".img-full-screen");
-const testArray = [...test];
-
-let placeHolder = document.querySelector(".img-placeholder");
-let currentIndex = 0;
-
-testArray.forEach((element, index) => {
-  element.style.cursor = "pointer";
-
-  element.addEventListener("click", () => {
-    console.log("click");
-    currentIndex = index;
-    console.log(currentIndex);
-    let testImg = element.querySelector("img").getAttribute("src");
-    placeHolder.style.display = "block";
-    placeHolder.style.backgroundImage = `url(${testImg})`;
-
-    document.body.style.overflow = "hidden";
-  });
-});
-
-rightArrow.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % testArray.length;
-  console.log(currentIndex);
-
-  showImage();
-});
-
-leftArrow.addEventListener("click", () => {
-  currentIndex = (currentIndex - 1 + testArray.length) % testArray.length;
-  console.log(currentIndex);
-  showImage();
-});
-
-/**************************/
-/* Function to Display Image */
-/**************************/
-
-function showImage() {
-  let testImg = testArray[currentIndex]
-    .querySelector("img")
-    .getAttribute("src");
-  placeHolder.style.backgroundImage = `url(${testImg})`;
-}
-
-// /**************************/
-// /* OPEN MODAL*/
-// /**************************/
-
-// const test = document.querySelectorAll(".img-full-screen");
-// const testArray = [...test];
-
-// let placeHolder = document.querySelector(".img-placeholder");
-// let currentIndex = 0;
-
-// testArray.forEach((element, index) => {
-//   element.style.cursor = "pointer";
-
-//   element.addEventListener("click", () => {
-//     console.log("click");
-//     currentIndex = index;
-//     console.log(currentIndex);
-//     let testImg = element.querySelector("img").getAttribute("src");
-//     // showImage();
-//     placeHolder.style.display = "block";
-//     placeHolder.style.backgroundImage = `url(${testImg})`;
-
-//     document.body.style.overflow = "hidden";
-//   });
-// });
-
-// /**************************/
-// /* CLOSE MODAL*/
-// /**************************/
-
-// /**************************/
-// /* NEXT IMAGE*/
-// /**************************/
 
 // /**************************/
 // /* EXIT BUTTON*/
@@ -183,18 +96,68 @@ exitButton.appendChild(svgElement);
 
 document.querySelector(".img-placeholder").appendChild(exitButton);
 
-// /**************************/
-// /* EXIT FUNCTION*/
-// /**************************/
+/**************************/
+/* OPEN MODAL*/
+/**************************/
+
+const test = document.querySelectorAll(".img-full-screen");
+const testArray = [...test];
+
+let placeHolder = document.querySelector(".img-placeholder");
+let currentIndex = 0;
+
+testArray.forEach((element, index) => {
+  element.style.cursor = "pointer";
+
+  element.addEventListener("click", () => {
+    console.log("click");
+    currentIndex = index;
+    console.log(currentIndex);
+    let testImg = element.querySelector("img").getAttribute("src");
+    placeHolder.style.display = "block";
+    placeHolder.style.backgroundImage = `url(${testImg})`;
+
+    document.body.style.overflow = "hidden";
+  });
+});
+
+/**************************
+/ * CLOSE MODAL*/
+/**************************/
 
 svgElement.addEventListener("click", () => {
   document.body.style.overflow = "auto";
   placeHolder.style.display = "none";
 });
-// /**************************/
-// /* LEFT ARROW */
-// /**************************/
 
-// /**************************/
-// /* RIGHT ARROW */
-// /**************************/
+/**************************/
+/* DISPLAY IMAGE*/
+/**************************/
+
+function showImage() {
+  let testImg = testArray[currentIndex]
+    .querySelector("img")
+    .getAttribute("src");
+  placeHolder.style.backgroundImage = `url(${testImg})`;
+}
+
+/**************************/
+/* NEXT IMAGE*/
+/**************************/
+
+rightArrow.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % testArray.length;
+  console.log(currentIndex);
+
+  showImage();
+});
+
+/**************************/
+/* PREVIOUS IMAGE*/
+/**************************/
+
+leftArrow.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + testArray.length) % testArray.length;
+  console.log(currentIndex);
+  showImage();
+});
